b2da8c46c87374bd6784972a8880208b92cf163e
vidiyalarasan <vidiyalarasans@gmail.com>
2026-01-31 23:49:15 +0530

    Add student form (POST)

--- docs/script.js
+++ docs/script.js
@@ -15,3 +15,26 @@ function loadStudents() {
         })
         .catch(error => console.error("Error:", error));
 }
+function addStudent() {
+    const student = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        course: document.getElementById("course").value
+    };
+
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(student)
+    })
+    .then(() => {
+        loadStudents();
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("course").value = "";
+    })
+    .catch(err => console.error(err));
+}
+
