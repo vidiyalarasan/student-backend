fe09a1dfc714ac0e25120a89876e1b7104260950
vidiyalarasan <vidiyalarasans@gmail.com>
2026-01-31 22:03:07 +0530

    Delete src directory

--- src/main/java/com/example/student/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.student.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(Long id) {
-        super("Student not found with id: " + id);
-    }
-}
